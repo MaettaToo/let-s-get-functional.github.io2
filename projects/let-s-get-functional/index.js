4cb2b397c1e93f3014153888a83455c4d395e1eb
MaettaToo <maetta.green2@gmail.com>
2025-05-08 12:06:06 -0500

    need help

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -173,26 +173,26 @@ var friendsCount = (array, name) => {
  //E: none 
  // use filter, then iterate over the friends array to see if the input name exists 
  // use 
- //return  _.filter(array, (curr) =>{
+ return  _.filter(array, (curr) =>{
  // const results = _.filter(curr.friends, (item) =>{
      //if(item.name === name){
        //return true;
      //};
 
-    //let results = [];
+    let results = [];
    // return results;
-    //for (let i = 0; i < curr.friends.length; i++){
-    //if(curr.friends[i].name === name){
-      //console.log(curr);
-       //results.push(curr);
-   //}
-    //console.log(results);
-    //}
+    for (let i = 0; i < curr.friends.length; i++){
+    if(curr.friends[i].name === name){
+       console.log(curr);
+       results.push(curr);
+   }
+    console.log(results);
+    }
     //console.log(results);
     //if(curr.friends.includes(name)){
       //return true;
     //}
-  //})
+  })
 
 };
 
